376c78841db58448501acef25424d3c2a4b1b5b8
satyam-kalihari <satyamkalihari17@gmail.com>
2025-07-24 18:51:22 +0530

    changed bg colour

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,16 @@
 "use client";
 
 import Hero from "@/components/Hero";
+import { OurServices } from "@/components/OurServices";
 import OurStory from "@/components/OurStory";
 
 
 export default function Home() {
   return (
-    <div className="font-serif bg-amber-50">
+    <div className="font-serif bg-pink-50">
       <Hero />
       <OurStory />
+      <OurServices />
     </div>
   );
 }
