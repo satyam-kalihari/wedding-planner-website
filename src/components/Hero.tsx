a224072b598a601973eac61c1355f4e8f137ed1f
satyam-kalihari <satyamkalihari17@gmail.com>
2025-08-07 17:48:21 +0530

    hero mobile layout

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,12 +8,12 @@ const Hero = () => {
       id="hero"
       className="h-[100vh] w-full flex flex-col gap-8 items-center justify-center bg-[url(/images/hero-image-3.jpg)] md:bg-[url(/images/hero-image-3.jpg)] md:bg-fixed bg-cover bg-center bg-no-repeat "
     >
-      <motion.div className="text-center text-5xl md:text-7xl text-[#fffbeb] font-sans font-extrabold">
+      <motion.div className="text-center text-4xl md:text-7xl text-[#fffbeb] font-sans font-extrabold">
         Your Dream Event And Wedding,
         <br />
         Perfectly Planned
       </motion.div>
-      <motion.div className="text-center text-2xl md:text-3xl md:font-medium font-medium text-[#ea5285]">
+      <motion.div className="text-center text-xl md:text-3xl md:font-medium font-medium text-[#ea5285]">
         Crafting Unforgettable Moments, One Wedding at a Time
       </motion.div>
       <motion.div className="flex items-center justify-center gap-8">
@@ -21,7 +21,7 @@ const Hero = () => {
           <motion.div
             whileHover={{ scale: 1.1 }}
             whileTap={{ scale: 0.9 }}
-            className="bg-[#ea5285] w-[170px] text-center cursor-default p-0.5 px-1.5 rounded-xs border-2 border-[#ea5285] "
+            className="bg-[#ea5285] text-sm md:text-lg w-[140px] md:w-[175px] text-center cursor-default p-0.5 px-1.5 rounded-xs border-2 border-[#ea5285] "
           >
             Discover Our Services
           </motion.div>
@@ -30,7 +30,7 @@ const Hero = () => {
           <motion.div
             whileHover={{ scale: 1.1 }}
             whileTap={{ scale: 0.9 }}
-            className="border-2 w-[170px] text-center border-[#fffbeb] p-0.5 px-1.5 rounded-xs backdrop-blur-xs "
+            className="border-2 text-sm md:text-lg w-[140px] md:w-[175px] text-center border-[#fffbeb] p-0.5 px-1.5 rounded-xs backdrop-blur-xs "
           >
             View Our Work
           </motion.div>
